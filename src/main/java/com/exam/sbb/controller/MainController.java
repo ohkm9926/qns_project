258254ca6d62ea2bfd1f7af48b92b0c6d8fdfabb
ohkm9926 <ohkm9926@naver.com>
2024-07-02 16:53:20 +0900

    getmapping,postmapping,requestmapping의 차이

--- src/main/java/com/exam/sbb/controller/MainController.java
+++ src/main/java/com/exam/sbb/controller/MainController.java
@@ -1,8 +1,7 @@
 package com.exam.sbb.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
@@ -13,5 +12,46 @@ public class MainController {
     public  String index(){
       return  "안녕하세요12";
   }
+
+  @GetMapping("/test")
+  @ResponseBody
+  public String showMain(){
+    return """
+            <h1>안녕하세요<h1/>
+            <input type ="text" placeholder="입력해주세요."/>
+            """;
+  }
+  @GetMapping("/get")
+  @ResponseBody
+  public String showGet(){
+    return """
+            <form method ="POST" action ="/page2">
+            <input type ="number" name ="age" placeholder = "나이입력" />
+            <input type ="submit" value ="page2로 Post방식으로 이동" />
+            </form>
+            """;
+  }
+
+  @PostMapping ("/page2")
+  @ResponseBody
+  public String showPost(@RequestParam(defaultValue = "0") int age){
+    return """
+            <h1>입력나이 : %d</h1>
+            <h1>여기는 post방식 페이지입니다.<h1/>
+            
+            """.formatted(age);
+  }
+
+  @GetMapping ("/page1")
+  @ResponseBody
+  public String showPost1(@RequestParam(defaultValue = "0") int age){
+    return """
+            <h1>입력나이 : %d</h1>
+            <h1>여기는 get방식 페이지입니다.<h1/>
+            
+            """.formatted(age);
+  }
+
+
 
 }
