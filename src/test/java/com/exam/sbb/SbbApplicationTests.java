7bc3bed37edd1025ae0846ee9734fcc320506e74
ohkm9926 <ohkm9926@naver.com>
2024-07-03 14:21:36 +0900

    findAll() 조회기능

--- src/test/java/com/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/exam/sbb/SbbApplicationTests.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -22,5 +25,13 @@ class SbbApplicationTests {
 		this.questionRepository.save(q1);
 	}
 
+    @Test
+	void testJpa2(){
+		List<Question> all = questionRepository.findAll();
+		assertEquals(2,all.size());
 
+		Question q = all.get(0);
+		//실행한 결과가 같은지 알려주는 assertEquals
+		assertEquals("kyumin이 무엇인가요",q.getSubject());
+	}
 }
