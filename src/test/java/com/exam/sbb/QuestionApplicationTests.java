3aa4448b14e6d1df8717136e15f0808948f7029f
ohkm9926 <ohkm9926@naver.com>
2024-07-05 17:23:57 +0900

    @BeforeEach test 공부용

---
+++ src/test/java/com/exam/sbb/QuestionApplicationTests.java
@@ -0,0 +1,130 @@
+package com.exam.sbb;
+
+import com.exam.sbb.question.Question;
+import com.exam.sbb.question.QuestionRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class QuestionApplicationTests {
+	@Autowired
+	private QuestionRepository questionRepository;
+
+	@BeforeEach
+	void beforeEach() {
+		clearData();
+		clearSampleData();
+
+
+	}
+
+
+
+	private void clearData() {
+
+		questionRepository.disableForeignKeyChecks();
+		questionRepository.truncate();
+		questionRepository.enableForeignKeyChecks();
+
+	}
+
+	private void clearSampleData() {
+
+		Question q1 = new Question();
+		q1.setSubject("kyumin이 무엇인가요");
+		q1.setContent("kyumin에 대해 알고싶어요");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);
+
+
+
+		Question q2 = new Question();
+		q2.setSubject("kyumin123이 무엇인가요");
+		q2.setContent("kyumin123에 대해 알고싶어요");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2);
+
+
+	}
+
+
+	@Test
+	void 저장(){
+
+
+		Question q1 = new Question();
+		q1.setSubject("kyumin이 무엇인가요");
+		q1.setContent("kyumin에 대해 알고싶어요");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);
+
+
+
+		Question q2 = new Question();
+		q2.setSubject("kyumin123이 무엇인가요");
+		q2.setContent("kyumin123에 대해 알고싶어요");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2);
+
+
+
+	}
+	/*
+
+    @Test
+	void testJpa2(){
+		List<Question> all = questionRepository.findAll();
+		assertEquals(2,all.size());
+
+		Question q = all.get(0);
+		//실행한 결과가 같은지 알려주는 assertEquals
+		assertEquals("kyumin이 무엇인가요",q.getSubject());
+	}
+
+	@Test
+	void testJpa3(){
+		Question q = questionRepository.findBySubject("kyumin123이 무엇인가요");
+	}
+
+	@Test
+	void testJpa4(){
+		Question q = questionRepository.findBySubjectAndContent("kyumin123이 무엇인가요","kyumin123에 대해 알고싶어요");
+		assertEquals(3,q.getId());
+	}
+
+	@Test
+	void testJpa5(){
+		List<Question> qList = questionRepository.findBySubjectLike("kyumin123%");
+		Question q = qList.get(0);
+		assertEquals("kyumin123이 무엇인가요" , q.getSubject());
+	}
+	@Test
+	void testJpa6(){
+		Optional<Question> oq = questionRepository.findById(1);
+		assertThat(oq.isPresent());
+		Question q = oq.get();
+		q.setSubject("수정된 제목");
+		questionRepository.save(q);
+	}
+	@Test
+	void testJpa7(){
+	   assertEquals(3 , questionRepository.count());
+	   Optional<Question> oq  = questionRepository.findById(1);
+	   assertTrue(oq.isPresent());
+	   Question q = oq.get();
+	   questionRepository.delete(q);
+	   assertEquals(2,questionRepository.count());
+	}
+
+ */
+}
